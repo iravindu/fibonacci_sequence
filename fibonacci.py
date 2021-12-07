da73b679de6381c8de13e3924ba944c5ffa87537
Ravindu Abeygunasekara <r.c.abeygunasekara@gmail.com>
2021-12-07 23:58:17 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+fibonacci = []
+n = int(input("Enter a number: "))
+for i in range(0,n):
+   #element = int(input("Enter an element : "))
+   if i==0:
+      element = 0
+      fibonacci.append(element)
+   elif i<=2 and i!=0:
+      element = 1
+      fibonacci.append(element)
+   else:
+      element = fibonacci[i-1]+fibonacci[i-2]
+      fibonacci.append(element)
+         
+print(fibonacci)   
